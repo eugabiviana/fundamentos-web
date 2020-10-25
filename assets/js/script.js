531a21f27f9d1ff093e4b7c3e2f9c43e5eff54cb
eugabiviana <qqcoisa@g>
2020-10-25 20:36:42 -0300

    Implementing Java Script

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*
+Case sensitive = reconhece letras maiúsculas e minúsculas
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.getElementById('nome')
+let email = document.querySelector('#email')
+ let assunto = document.querySelector('#assunto')
+ let redeSocial = document.querySelector('#redeSocial')
+
+ nome.style.width = '100%'
